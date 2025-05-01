0a7ba8d0176b47fa2dc4b1f8c376885b1fe1044b
Amar <arampers94@gmail.com>
2025-04-30 22:38:58 -0400

    updated time off event endpoint names

--- src/routes/time-off-events/time-off-events.routes.ts
+++ src/routes/time-off-events/time-off-events.routes.ts
@@ -11,12 +11,8 @@ import {
 export const router: express.Router = express.Router();
 
 router.route("/:id").get(getTimeOffEventById);
-router
-  .route("/current-time-off-events/:teamId")
-  .get(getCurrentTimeOffEventsByTeamId);
-router
-  .route("/future-time-off-events/:teamId")
-  .get(getFutureTimeOffEventsByTeamId);
-router.route("/time-off-event").post(createTimeOffEvent);
+router.route("/current/:teamId").get(getCurrentTimeOffEventsByTeamId);
+router.route("/future/:teamId").get(getFutureTimeOffEventsByTeamId);
+router.route("/").post(createTimeOffEvent);
 router.route("/:id").put(updateTimeOffEvent);
 router.route("/:id").delete(deleteTimeOffEvent);
